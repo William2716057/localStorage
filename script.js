e41b6fbfd1c034b3d03d1cfa208c4b823050e869
User <Willspri@gmail.com>
2025-03-21 21:37:15 +1300

    update

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function saveData() {
     }
 }
 
-// Function to display saved data from local storage
-function displayData() {
-    let savedData = localStorage.getItem("storedData");
-    if (savedData) {
-        document.getElementById("savedData").textContent = "Saved Data: " + savedData;
-    }
-}
+// Display saved data from local storage
+//function displayData() {
+//    let savedData = localStorage.getItem("storedData");
+//    if (savedData) {
+//        document.getElementById("savedData").textContent = "Saved Data: " + savedData;
+//    }
+//}
 
 // Load saved data on page load
-window.onload = displayData;
+//window.onload = displayData;
